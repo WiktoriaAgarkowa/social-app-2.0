7fbc465160b138728c1b032ec658a6b405b477e1
wagarkowa <victoriaagarkova96@gmail.com>
2021-06-01 18:40:50 +0200

    created Input and Button smart components, created Login and SignUp forms

--- src/pages/log-in/log-in.component.jsx
+++ src/pages/log-in/log-in.component.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import FormInput from '../../components/form-input/form-input.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
 
-const Login = () => (
-    <div>
-        <h1>LOGIN</h1>
-    </div>
-)
+import './log-in.styles.scss';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({[name]: value})
+    }
+
+
+    render() {
+        return (
+            <div className='container'>
+
+                <h1 className='header-login'>LOGIN</h1>
+        
+                <form action="">
+
+                <FormInput 
+                    type="text" 
+                    name="username" 
+                    value={this.state.username}
+                    label="username"
+                    handleChange={this.handleChange}
+                    required/>
+                   
+                   <FormInput 
+                    type="password" 
+                    name="password" 
+                    value={this.state.password}
+                    label="password"
+                    handleChange={this.handleChange}
+                    required/>
+
+                    <CustomButton>LOGIN IN</CustomButton>
+
+                </form>
+
+                <p className='prompt'>Use name: Adam, password: 1234</p>
+            </div>
+        )
+    }
+}
 
 export default Login;
--- src/pages/sign-up/sign-up.component.jsx
+++ src/pages/sign-up/sign-up.component.jsx
@@ -1,9 +1,74 @@
 import React from 'react';
+import FormInput from '../../components/form-input/form-input.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
 
-const SignUp = () => (
-    <div>
-        <h1>SignUp</h1>
-    </div>
-)
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            confirm: ''
+        }
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({[name]: value})
+    }
+
+
+    render() {
+        return (
+            <div className='container'>
+
+                <h1 className='header-login'>SIGN UP</h1>
+        
+                <form action="">
+
+                <FormInput 
+                    type="text" 
+                    name="username" 
+                    value={this.state.username}
+                    label="username"
+                    handleChange={this.handleChange}
+                    required/>
+
+                <FormInput 
+                    type="email" 
+                    name="email" 
+                    value={this.state.email}
+                    label="email"
+                    handleChange={this.handleChange}
+                    required/>    
+                   
+                <FormInput 
+                    type="password" 
+                    name="password" 
+                    value={this.state.password}
+                    label="password"
+                    handleChange={this.handleChange}
+                    required/>
+
+                <FormInput 
+                    type="password" 
+                    name="confirm password" 
+                    value={this.state.confirm}
+                    label="confirm"
+                    handleChange={this.handleChange}
+                    required/>
+
+                    <CustomButton>SIGN UP</CustomButton>
+
+                </form>
+            </div>
+        )
+    }
+}
 
 export default SignUp;
